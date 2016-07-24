ddb0c0297b51de81bba2f4b627f719c4702bdcb2
None <stok20010711@gmail.com>
2016-07-24 01:17:57 +0000

    update

--- test/gem_minitest_test.rb
+++ test/gem_minitest_test.rb
@@ -9,6 +9,7 @@ class GemMinitestTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::GemMinitest::VERSION
   end
+
  # def test_it_does_something_useful
  #   assert false
  # end
